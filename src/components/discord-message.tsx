34ee3e38bab215963eb0b128cb9c6076ecb70aa9
george-garcia <jorge.a.garcia12@gmail.com>
2025-08-12 03:20:41 -0500

    started work on the bento
    left part of bento grid finished

--- src/components/discord-message.tsx
+++ src/components/discord-message.tsx
@@ -63,12 +63,12 @@ export const DiscordMessage = ({
         </div>
 
         <div className='bg-[#2f3136] text-sm w-full rounded p-3 mb-4 mt-1.5'>
-          <div className='flex flex-col sm:flex-row sm:items-center sm:justify-between mb-2'>
+          <div className='flex flex-row items-center justify-between mb-2'>
             {badgeText ? (
               <span
                 className={cn(
                   "inline-flex order-2 items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset",
-                  getBadgeStyles("#faa61a")
+                  getBadgeStyles(badgeColor)
                 )}
               >
                 {badgeText}
